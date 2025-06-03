6aa5f655e1c023bbfa9d77c24cf7c68f70a9407a
Ansel Rognlie <ansel@adadevelopersacademy.org>
2025-06-03 15:51:09 -0700

    adds live code result

--- src/index.js
+++ src/index.js
@@ -17,7 +17,16 @@ const API_KEY = process.env.api_key;
 // const API_KEY = process.env.API_KEY; // Access the API_KEY from .env file
 
 // single location call example
-const placeName = 'Great Wall of China';
+// const placeName = 'Great Wall of China';
+const places = [
+  'Great Wall of China',
+  'Petra',
+  'Colosseum',
+  'Chichen Itza',
+  'Machu Picchu',
+  'Taj Mahal',
+  'Christ the Redeemer',
+];
 
 // Endpoint documentation: https://docs.locationiq.com/reference/search
 const BASE_URL = 'https://us1.locationiq.com/v1/search';
@@ -29,19 +38,88 @@ const BASE_URL = 'https://us1.locationiq.com/v1/search';
 // the Great Wall of China. We'll use this to form the basis of our
 // solutions.
 
-axios
-  .get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: placeName,
-      format: 'json',
-    },
-  })
-  .then(response => {
-    const { lat, lon } = response.data[0];
-    console.log({ lat, lon });
-    return;  // as configured, eslint requires a return statement
-  })
-  .catch(error => {
-    console.log(error);
+// input: placeName: string
+// returns: Promise<Location{}>
+const getLatLonForPlace = (placeName) => {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: placeName,
+        format: 'json',
+      },
+    })
+    .then(response => {
+      const { lat, lon } = response.data[0];
+      // console.log({ lat, lon });
+      return { lat, lon };
+    });
+};
+
+const wait = (ms) => { return new Promise(resolve => { setTimeout(resolve, ms); }); };
+
+// input: places: string[]
+// returns: Promise<Result{}>
+const getLatLonForPlaces = async (places) => {
+  // result
+  const result = {};
+
+  // for each wonder
+  for (const place of places) {
+    // call api with wonder
+    const loc = await getLatLonForPlace(place);
+    // add result to final structure
+    result[place] = loc;
+
+    await wait(500);
+  }
+
+  return result;
+};
+
+// returns: Promise<Result{}>
+const getLatLonForPlacesPromises = (places) => {
+  // result
+  const result = {};
+
+  let promise = Promise.resolve();
+
+  // for each wonder
+  for (const place of places) {
+    // call api with wonder
+    promise = promise
+      .then(() => {
+        return getLatLonForPlace(place);
+      })
+      .then(loc => {
+        // add result to final structure
+        result[place] = loc;
+        return;
+      })
+      .then(() => wait(500));
+  }
+
+  return promise.then(() => {
+    return result;
   });
+};
+
+// getLatLonForPlaces(places);
+
+// setTimeout(() => {
+//   console.log('bzzt!');
+// }, 5000);
+
+
+// wait(5000)
+//   .then(() => {
+//     console.log('promise');
+//     return;
+//   }).catch(() => {});
+
+getLatLonForPlaces(places)
+  .then((result) => {
+    console.log({ result });
+    return;
+  })
+  .catch(() => {});
